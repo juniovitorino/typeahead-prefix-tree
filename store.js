646600c508169e6231367611b9cc1a70b5f44da4
Junio Vitorino <jgvitorino@gmail.com>
2022-10-14 21:35:48 -0300

    fix: Loading the entire file to memory before parse it.

--- store.js
+++ store.js
@@ -3,14 +3,20 @@ const NamesData = fs.createReadStream('./data/names.json');
 
 const { PrefixTree } = require('./lib/PrefixTree')
 const prefixTree = new PrefixTree()
+let file = '';
 
 NamesData.on('data', (chunk) => {
-  const data = JSON.parse(chunk.toString())
+  file += chunk.toString()
+});
+
+NamesData.on('end', () => {
+  const data = JSON.parse(file)
+
   Object.keys(data).map(key => {
     const times = parseInt(data[key])
     if (!isNaN(times)) prefixTree.insert({ key, times })
   })
-});
+})
 
 NamesData.on('error', err => { throw err })
 
